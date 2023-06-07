ec84f22c9a319cca2632efb9aa58d6cf6b89a4f6
sad1400 <106877461+sad1400@users.noreply.github.com>
2023-06-07 18:25:43 +0300

    Delete lab_7js directory

--- lab_7js/lab_7.1.js
+++
@@ -1,7 +0,0 @@
-let selectedOption = music.options[music.selectedIndex];
-console.log( music.options[music.selectedIndex] );
-console.log( selectedOption.value );
-console.log( selectedOption.text );
-let newOption = new Option("Классика", "Classic");
-music.append(newOption);
-newOption.selected = true;
