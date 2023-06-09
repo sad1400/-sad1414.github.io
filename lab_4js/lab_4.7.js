347c0267e00378251bbc98218fb79e1cbce6b43b
sad1400 <106877461+sad1400@users.noreply.github.com>
2023-06-09 13:27:32 +0300

    Update lab_4.7.js

--- lab_4js/lab_4.7.js
+++ lab_4js/lab_4.7.js
@@ -1,13 +1,16 @@
- function formatDate(date) {
-            let day = date.getDate();
-            let month = date.getMonth();
-            let year = date.getFullYear().toString().slice(2);
-
-            if (day < 10) day = '0' + day;
-            if (month < 10) month = '0' + month;
-
-            return day + '.' + month + '.' + year;
-        }
-
-        let date = new Date(2023, 06, 3);
-        console.log(formatDate(date)); 
+let date = new Date();
+function formatDate(date){
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear() % 100;
+    let res = ' ';
+    if (day < 10){
+        day = '0' + day;
+    }
+    if (month < 10){
+        month = '0' + month;
+    }
+    res = day + '.' + month + '.' + year;
+    return res;
+}
+console.log(formatDate(date));
