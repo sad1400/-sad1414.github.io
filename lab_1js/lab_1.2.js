428cd2eaaa59d4745555d53b68ca0ab3428c1ed6
sad1400 <106877461+sad1400@users.noreply.github.com>
2023-06-09 11:39:23 +0300

    Update lab_1.2.js

--- lab_1js/lab_1.2.js
+++ lab_1js/lab_1.2.js
@@ -1,22 +1,35 @@
-let num = +prompt("Количество ворон");
-if (num == 1){ 
-  alert(`На ветке сидит ${num} ворона`);
-}
-else if (num >= 2 && num <= 4){
-  alert(`На ветке сидит ${num} вороны`);
-}
-else if (num >= 5 && num < 20){
-  alert(`На ветке сидит ${num} ворон`);
-}
-else if (num >= 20){
-  let ost = num % 10;
-  if (ost == 1){ 
-  alert(`На ветке сидит ${num} ворона`);
-}
-else if (ost >= 2 && ost <= 4){
-  alert(`На ветке сидит ${num} вороны`);
-}
-else if (ost >= 5 && ost < 20){
-  alert(`На ветке сидит ${num} ворон`);
-}
-}
+let num = prompt("Введите целое число", "");
+        if (num % 100 >= 11 && num % 100 <= 14) {
+            switch (num % 100) {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    alert(`На ветке сидит ${num} ворон`)
+                    break;
+            }
+        }
+        else {
+            switch (num % 10) {
+                case 0:
+                    alert(`На ветке сидит ${num} ворон`)
+                    break;
+                case 1:
+                    alert(`На ветке сидит ${num} ворона`)
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    alert(`На ветке сидит ${num} вороны`)
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 9:
+                    alert(`На ветке сидит ${num} ворон`)
+                    break;
+                default:
+                    alert(`Число нецелое или это не число`)
+                    break;
+            }
+        }
